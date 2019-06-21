aa991cbf8d9c7ea32cbeb6e7b25da76b2c7e75e0
Laboratorios <Laboratorios@Q07>
2019-06-20 20:09:07 -0600

    Hello world consola

---
+++ HelloWorldConsola/src/helloworldconsola/HelloWorldConsola.java
@@ -0,0 +1,12 @@
+package helloworldconsola;
+
+public class HelloWorldConsola {
+
+    private static void imprimirMensaje() {
+        System.out.print("Hello world!");
+    }
+
+    public static void main(String[] args) {
+        imprimirMensaje();
+    }
+}
